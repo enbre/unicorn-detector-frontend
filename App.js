71dbbea1619cb7149f342f4a4b6e2abec29861bf
enbre <noahbrezel@gmail.com>
2020-12-13 22:41:03 -0800

    adds initial navigation

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 // react imports
 import React, { useState, useEffect } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
-
-
+import {NavigationContainer, StackActions} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 //expo module imports
 import { LinearGradient } from 'expo-linear-gradient'
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions'
 import * as Font from 'expo-font'
-import { loadAsync } from 'expo-font';
+// import { loadAsync } from 'expo-font';
 // import { useFonts, Lakki Reddy, Comic Nue } from @expo-google-fonts/inter
 // file imports
+import Home from './src/components/Home'
+import Unicorn from './src/components/Unicorn'
+import Sightings from './src/components/Sightings'
 import UnicornModel from './src/models/unicorn'
 import SightingModel from './src/models/sighting'
 import { API_KEY } from './utils/weatherKey'
-import logo from './assets/logo.png'
+// import logo from './assets/logo.png'
 
 
+const Root = createStackNavigator()
 
 export default function App() {
   // state:
@@ -65,29 +69,29 @@ export default function App() {
     setLon(userLocation.coords.longitude)
   }
 
-  useEffect(() => {
-    // console.log("latitude state:", lat)
-    // console.log("longitude state:", lon)
-    // console.log("-----------")
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`)
-      .then(res => res.json())
-      .then(json => {
-        setWeather(json)
-      });
-    if (weather) {
-      // redundant api request to address asynchronous attempts to set state
-      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`)
-        .then(res => res.json())
-        .then(json => {
-          // console.log('all weather data:', json);
-          setTemp(Math.round(json.main.temp))
-          setWind(Math.round(json.wind.speed))
-        });
-    }
-    // console.log('wind state:', wind);
-    // console.log('temp state:', temp);
-    // console.log("-----------")
-  }, [lat, lon])
+  // useEffect(() => {
+  //   // console.log("latitude state:", lat)
+  //   // console.log("longitude state:", lon)
+  //   // console.log("-----------")
+  //   fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`)
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       setWeather(json)
+  //     });
+  //   if (weather) {
+  //     // redundant api request to address asynchronous attempts to set state
+  //     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`)
+  //       .then(res => res.json())
+  //       .then(json => {
+  //         // console.log('all weather data:', json);
+  //         setTemp(Math.round(json.main.temp))
+  //         setWind(Math.round(json.wind.speed))
+  //       });
+  //   }
+  //   // console.log('wind state:', wind);
+  //   // console.log('temp state:', temp);
+  //   // console.log("-----------")
+  // }, [lat, lon])
 
   // // alternate get weather function
   // useEffect(() => {
@@ -109,61 +113,61 @@ export default function App() {
   //   console.log("-----------")
   // }, [lat, lon])
 
-  useEffect(() => {
-    console.log('wind state:', wind);
-    console.log('temp state:', temp);
-    console.log("-----------")
-    const pickUnicornId = async () => {
-      if (temp === 53) {
-        setUnicornId(3)
-      }
-      else if (temp === 53) {
-        setUnicornId(4)
-      }
-      else if (temp === 57) {
-        setUnicornId(5)
-      }
-      else if (temp === 59) {
-        setUnicornId(6)
-      }
-      else if (temp === 61) {
-        setUnicornId(7)
-      }
-      else if (temp === 63) {
-        setUnicornId(8)
-      }
-      else if (temp === 65) {
-        setUnicornId(9)
-      }
-      else if (temp === 67) {
-        setUnicornId(10)
-      }
-      else if (temp === 69) {
-        setUnicornId(11)
-      }
-      else if (temp === 71) {
-        setUnicornId(12)
-      }
-      else if (temp === 73) {
-        setUnicornId(13)
-      }
-      else if (temp === 75) {
-        setUnicornId(14)
-      }
-      else if (temp === 77) {
-        setUnicornId(15)
-      }
-      else if (temp === 79) {
-        setUnicornId(16)
-      }
-      else if (temp === 81) {
-        setUnicornId(17)
-      }
-      else {
-        setUnicornId(2)
-      }
-    }
-  }, [temp])
+  // useEffect(() => {
+  //   console.log('wind state:', wind);
+  //   console.log('temp state:', temp);
+  //   console.log("-----------")
+  //   const pickUnicornId = async () => {
+  //     if (temp === 53) {
+  //       setUnicornId(3)
+  //     }
+  //     else if (temp === 53) {
+  //       setUnicornId(4)
+  //     }
+  //     else if (temp === 57) {
+  //       setUnicornId(5)
+  //     }
+  //     else if (temp === 59) {
+  //       setUnicornId(6)
+  //     }
+  //     else if (temp === 61) {
+  //       setUnicornId(7)
+  //     }
+  //     else if (temp === 63) {
+  //       setUnicornId(8)
+  //     }
+  //     else if (temp === 65) {
+  //       setUnicornId(9)
+  //     }
+  //     else if (temp === 67) {
+  //       setUnicornId(10)
+  //     }
+  //     else if (temp === 69) {
+  //       setUnicornId(11)
+  //     }
+  //     else if (temp === 71) {
+  //       setUnicornId(12)
+  //     }
+  //     else if (temp === 73) {
+  //       setUnicornId(13)
+  //     }
+  //     else if (temp === 75) {
+  //       setUnicornId(14)
+  //     }
+  //     else if (temp === 77) {
+  //       setUnicornId(15)
+  //     }
+  //     else if (temp === 79) {
+  //       setUnicornId(16)
+  //     }
+  //     else if (temp === 81) {
+  //       setUnicornId(17)
+  //     }
+  //     else {
+  //       setUnicornId(2)
+  //     }
+  //   }
+  // }, [temp])
 
 
   // find one unicorn. currently hard-coded id#
@@ -172,7 +176,7 @@ export default function App() {
     console.log(res.unicorn)
     setUnicorn(res.unicorn)
   }
-  
+
   // find all sightings
   const findAllSightings = async () => {
     const res = await SightingModel.all()
@@ -193,82 +197,97 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <LinearGradient colors={['transparent', 'white']} style={styles.backgroundGradient} />
-      <Text style={styles.title}>
-        Unicorn Detector
-        </Text>
-      <TouchableOpacity
+    <NavigationContainer>
+      <Root.Navigator initialRouteName="Home"> 
+        <Root.Screen name="Home" component={Home} />
+        <Root.Screen name="Unicorn" component={Unicorn} />
+        <Root.Screen name="Sightings" component={Sightings} />
+      </Root.Navigator> 
+    </NavigationContainer>
+  
+  
 
-        onPress={getLocation}
-      >
-        <Image
-          // source={{uri:'https://static.wikia.nocookie.net/mlp/images/d/d2/UUM2_ID_S9E26.png/revision/latest?cb=20191013154637' }}
-          source={logo}
-          style={styles.logo}
-        />
-      </TouchableOpacity>
-      <Text style={styles.instructions}>Short press the logo to search for unicorns!</Text>
-      <TouchableOpacity
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Let's do something</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+
+
+
+
+
+    // <View style={styles.container}>
+    //   <LinearGradient colors={['transparent', 'white']} style={styles.backgroundGradient} />
+    //   <Text style={styles.title}>
+    //     Unicorn Detector
+    //     </Text>
+    //   <TouchableOpacity
+
+    //     onPress={getLocation}
+    //   >
+    //     <Image
+    //       // source={{uri:'https://static.wikia.nocookie.net/mlp/images/d/d2/UUM2_ID_S9E26.png/revision/latest?cb=20191013154637' }}
+    //       source={logo}
+    //       style={styles.logo}
+    //     />
+    //   </TouchableOpacity>
+    //   <Text style={styles.instructions}>Short press the logo to search for unicorns!</Text>
+    //   <TouchableOpacity
+    //     style={styles.button}
+    //   >
+    //     <Text style={styles.buttonText}>Let's do something</Text>
+    //   </TouchableOpacity>
+    //   {/* <StatusBar style="auto" /> */}
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgb(229,184,244)',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 275,
-    height: 275,
-    marginBottom: 10
-  },
-  title: {
-    color: 'rgba(129, 90, 159, 1)',
-    fontSize: 35,
-    // fontFamily: 'Chicle',
-    marginHorizontal: 15,
-    marginBottom: 75,
-    marginTop: -50
-  },
-  button: {
-    marginTop: 50,
-    backgroundColor: "white",
-    padding: 15,
-    borderRadius: 30,
-    borderWidth: 2,
-    borderColor: "rgba(129, 90, 159, 1)",
-  },
-  instructions: {
-    marginTop: 50,
-    color: 'rgba(129, 90, 159, 1)',
-    fontSize: 18,
-    // fontFamily: "ComicNeue"
-  },
-  buttonText: {
-    color: 'rgba(129, 90, 159, 1)',
-    fontSize: 18,
-    // fontFamily: "ComicNeue"
-  },
-  backgroundGradient: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    height: 800
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: "contain"
-  }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'rgb(229,184,244)',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   logo: {
+//     width: 275,
+//     height: 275,
+//     marginBottom: 10
+//   },
+//   title: {
+//     color: 'rgba(129, 90, 159, 1)',
+//     fontSize: 35,
+//     // fontFamily: 'Chicle',
+//     marginHorizontal: 15,
+//     marginBottom: 75,
+//     marginTop: -50
+//   },
+//   button: {
+//     marginTop: 50,
+//     backgroundColor: "white",
+//     padding: 15,
+//     borderRadius: 30,
+//     borderWidth: 2,
+//     borderColor: "rgba(129, 90, 159, 1)",
+//   },
+//   instructions: {
+//     marginTop: 50,
+//     color: 'rgba(129, 90, 159, 1)',
+//     fontSize: 18,
+//     // fontFamily: "ComicNeue"
+//   },
+//   buttonText: {
+//     color: 'rgba(129, 90, 159, 1)',
+//     fontSize: 18,
+//     // fontFamily: "ComicNeue"
+//   },
+//   backgroundGradient: {
+//     position: 'absolute',
+//     left: 0,
+//     right: 0,
+//     top: 0,
+//     height: 800
+//   },
+//   thumbnail: {
+//     width: 300,
+//     height: 300,
+//     resizeMode: "contain"
+//   }
 
-});
+// });
---
+++ src/components/Unicorn.js
@@ -0,0 +1,86 @@
+// react imports
+import React, { useState, useEffect } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+// expo imports
+import { LinearGradient } from 'expo-linear-gradient'
+import * as Font from 'expo-font'
+
+
+
+
+
+export default function Home({navigation}) {
+
+
+   return (
+      <View style={styles.container}>
+         <LinearGradient colors={['transparent', 'white']} style={styles.backgroundGradient} />
+         <Text style={styles.title}>
+            Unicorn Screen
+          </Text>
+         <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Home')}
+         >
+            <Text style={styles.buttonText}>link to home screen</Text>
+         </TouchableOpacity>
+         <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Sightings')}
+         >
+            <Text style={styles.buttonText}>Link to sightings screen</Text>
+         </TouchableOpacity>
+      </View>
+   );
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: 'rgb(229,184,244)',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+   title: {
+      color: 'rgba(129, 90, 159, 1)',
+      fontSize: 35,
+      // fontFamily: 'Chicle',
+      marginHorizontal: 15,
+      marginBottom: 75,
+      marginTop: -50
+   },
+   button: {
+      marginTop: 50,
+      backgroundColor: "white",
+      padding: 15,
+      borderRadius: 30,
+      borderWidth: 2,
+      borderColor: "rgba(129, 90, 159, 1)",
+   },
+   instructions: {
+      marginTop: 50,
+      color: 'rgba(129, 90, 159, 1)',
+      fontSize: 18,
+      // fontFamily: "ComicNeue"
+   },
+   buttonText: {
+      color: 'rgba(129, 90, 159, 1)',
+      fontSize: 18,
+      // fontFamily: "ComicNeue"
+   },
+   backgroundGradient: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      height: 800
+   },
+   thumbnail: {
+      width: 300,
+      height: 300,
+      resizeMode: "contain"
+   }
+
+});
